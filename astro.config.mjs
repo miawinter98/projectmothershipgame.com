909dfe0003ab201c65a98e215137f86995a1832e
Mia Rose Winter <mia@miawinter.de>
2025-12-27 01:17:58 +0100

    changed build directory to dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  // GitLab Pages requires exposed files to be located in a folder called "public".
-  // So we're instructing Astro to put the static build output in a folder of that name.
-  outDir: 'public',
   // The folder name Astro uses for static files (`public`) is already reserved
   // for the build output. So in deviation from the defaults we're using a folder
   // called `static` instead.
